04dacd0b48bc0417496d8071885dd8608375e3fd
Shibu TN <tnshibu@yahoo.com>
2020-08-10 03:39:04 +0530

    dockerize

---
+++ day-trader-java-app/daytrader-example-quotesrepo/daytrader-quotesapp/daytrader-quotes/docker-build.sh
@@ -0,0 +1,1 @@
+docker build --build-arg EXPOSE_PORT=4443 --build-arg DATABASE_DRIVER=org.apache.derby.jdbc.EmbeddedDriver --build-arg DATABASE_URL='jdbc:derby:tradesdb;create=true' -t daytrader-quotes:1.0 .
