2e361cf41bd968cc86ff5e6f5c6126c1b495af18
Shibu TN <tnshibu@yahoo.com>
2020-08-18 16:35:37 +0530

    docker images now use from dockerhub/tnshibu repository

---
+++ day-trader-java-app/run_all_servers_docker.sh
@@ -0,0 +1,10 @@
+cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-accountsrepo/daytrader-accountsapp/daytrader-accounts
+./docker-run.sh
+cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-gatewayrepo/daytrader-gatewayapp/daytrader-gateway
+./docker-run.sh
+cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-portfoliosrepo/daytrader-portfoliosapp/daytrader-portfolios
+./docker-run.sh
+cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-quotesrepo/daytrader-quotesapp/daytrader-quotes
+./docker-run.sh
+cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-webrepo/daytrader-webapp/daytrader-web
+./docker-run.sh
