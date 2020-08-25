6aa637f94429537f7c5345a50f503b2c4f18eb0d
Shibu TN <tnshibu@yahoo.com>
2020-08-25 17:36:33 +0530

    postman scripts

--- day-trader-java-app/daytrader-postman-testing/test_all.sh
+++ day-trader-java-app/daytrader-postman-testing/test_all.sh
@@ -1,4 +1,7 @@
 #put newman command shere
-newman run AdvanceAPIs.postman_collection.json --reporters cli,junit --reporter-junit-export Results\jadvanceAPIResult.xml
+#newman run AdvanceAPIs.postman_collection.json --reporters cli,junit --reporter-junit-export Results\jadvanceAPIResult.xml
+#newman run SimpleCURDAPIs.postman_collection.json --reporters cli,junit --reporter-junit-export Results\SimpleCURDAPIsResult.xml
 
-newman run SimpleCURDAPIs.postman_collection.json --reporters cli,junit --reporter-junit-export Results\SimpleCURDAPIsResult.xml
+newman run accounts.postman_collection.json  --reporters cli,junit --reporter-junit-export Results\accountsResult.xml
+newman run portfolio.postman_collection.json --reporters cli,junit --reporter-junit-export Results\portfolioResult.xml
+newman run quotes.postman_collection.json    --reporters cli,junit --reporter-junit-export Results\quotesResult.xml
