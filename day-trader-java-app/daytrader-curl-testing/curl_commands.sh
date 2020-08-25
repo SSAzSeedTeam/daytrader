7f4fc657783b33c7ac7ece649a28656e300e256c
arun.gx1.amirtharaj <arun.gx1.amirtharaj@gmail.com>
2020-08-25 11:47:34 +0000

    Updated curl_commands.sh

--- day-trader-java-app/daytrader-curl-testing/curl_commands.sh
+++ day-trader-java-app/daytrader-curl-testing/curl_commands.sh
@@ -11,38 +11,38 @@ curl -k https://localhost:5443
 #  RECREATE TABLES
 #-----------------
 #Accounts
-curl -k  -X POST "https://localhost:1443//admin/recreateDBTables"
-curl -k  -X POST "https://localhost:1443//admin/tradeBuildDB?limit=10&offset=0"
-curl -k  -X POST "https://localhost:1443//admin/tradeBuildDB?limit=10&offset=10"
-curl -k  -X POST "https://localhost:1443//admin/tradeBuildDB?limit=10&offset=20"
-curl -k  -X POST "https://localhost:1443//admin/tradeBuildDB?limit=10&offset=30"
+curl -k -L -X POST "https://localhost:1443/admin/recreateDBTables"
+curl -k -L -X POST "https://localhost:1443/admin/tradeBuildDB?limit=10&offset=0"
+curl -k -L -X POST "https://localhost:1443/admin/tradeBuildDB?limit=10&offset=10"
+curl -k -L -X POST "https://localhost:1443/admin/tradeBuildDB?limit=10&offset=20"
+curl -k -L -X POST "https://localhost:1443/admin/tradeBuildDB?limit=10&offset=30"
 
 #Portfolio
-curl -k  -X POST "https://localhost:3443/admin/recreateDBTables"
-curl -k  -X POST "https://localhost:3443/admin/tradeBuildDB?limit=10&offset=0"
-curl -k  -X POST "https://localhost:3443/admin/tradeBuildDB?limit=10&offset=10"
-curl -k  -X POST "https://localhost:3443/admin/tradeBuildDB?limit=10&offset=20"
-curl -k  -X POST "https://localhost:3443/admin/tradeBuildDB?limit=10&offset=30"
+curl -k  -L -X POST "https://localhost:3443/admin/recreateDBTables"
+curl -k  -L -X POST "https://localhost:3443/admin/tradeBuildDB?limit=10&offset=0"
+curl -k  -L -X POST "https://localhost:3443/admin/tradeBuildDB?limit=10&offset=10"
+curl -k  -L -X POST "https://localhost:3443/admin/tradeBuildDB?limit=10&offset=20"
+curl -k  -L -X POST "https://localhost:3443/admin/tradeBuildDB?limit=10&offset=30"
 
 #Quotes
-curl -k  -X POST "https://localhost:4443/admin/recreateDBTables"
-curl -k  -X POST "https://localhost:4443/admin/tradeBuildDB?limit=10&offset=0"
-curl -k  -X POST "https://localhost:4443/admin/tradeBuildDB?limit=10&offset=10"
-curl -k  -X POST "https://localhost:4443/admin/tradeBuildDB?limit=10&offset=20"
-curl -k  -X POST "https://localhost:4443/admin/tradeBuildDB?limit=10&offset=30"
+curl -k  -L -X POST "https://localhost:4443/admin/recreateDBTables"
+curl -k  -L -X POST "https://localhost:4443/admin/tradeBuildDB?limit=10&offset=0"
+curl -k  -L -X POST "https://localhost:4443/admin/tradeBuildDB?limit=10&offset=10"
+curl -k  -L -X POST "https://localhost:4443/admin/tradeBuildDB?limit=10&offset=20"
+curl -k  -L -X POST "https://localhost:4443/admin/tradeBuildDB?limit=10&offset=30"
 
 #=======================================================================
 #  POPULATE TABLES
 #-----------------
 
-curl -k  -X GET  "https://localhost:1443/accounts/uid:1/profiles"
-curl -k  -X GET  "https://localhost:1443/accounts/uid:1"
+curl -k  -L -X GET  "https://localhost:1443/accounts/uid:1/profiles"
+curl -k  -L -X GET  "https://localhost:1443/accounts/uid:1"
 
 #Portfolio
 #curl -k  -X GET  "https://localhost:3443/portfolios/{userId}/orders"
 #curl -k  -X GET  "https://localhost:3443/portfolios/{userId}"
 
 #Quotes
-curl -k  -X GET  "https://localhost:4443/quotes/s:1"
-curl -k  -X GET  "https://localhost:4443/quotes?limit=10"
+curl -k  -L -X GET  "https://localhost:4443/quotes/s:1"
+curl -k  -L -X GET  "https://localhost:4443/quotes?limit=10"
 ########################################################################
