a0256c3e36b97102d4fda9c0e5495889e53572b0
Puneet Singh <puneetsingh.gx@outlook.com>
2020-08-26 09:08:28 +0000

    Updated test_all.sh

--- day-trader-java-app/daytrader-postman-testing/test_all.sh
+++ day-trader-java-app/daytrader-postman-testing/test_all.sh
@@ -2,6 +2,6 @@
 #newman run AdvanceAPIs.postman_collection.json --reporters cli,junit --reporter-junit-export Results\jadvanceAPIResult.xml
 #newman run SimpleCURDAPIs.postman_collection.json --reporters cli,junit --reporter-junit-export Results\SimpleCURDAPIsResult.xml
 
-newman run accounts.postman_collection.json  --reporters cli,junit --reporter-junit-export Results\accountsResult.xml
-newman run portfolio.postman_collection.json --reporters cli,junit --reporter-junit-export Results\portfolioResult.xml
-newman run quotes.postman_collection.json    --reporters cli,junit --reporter-junit-export Results\quotesResult.xml
+newman run accounts.postman_collection.json  --insecure --reporters cli,junit --reporter-junit-export Results\accountsResult.xml
+newman run portfolio.postman_collection.json --insecure --reporters cli,junit --reporter-junit-export Results\portfolioResult.xml
+newman run quotes.postman_collection.json    --insecure --reporters cli,junit --reporter-junit-export Results\quotesResult.xml
