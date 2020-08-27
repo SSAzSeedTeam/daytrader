62e95335ec629b2f3ec258163fa1a38a08324d23
Shibu TN <tnshibu@yahoo.com>
2020-08-27 16:49:20 +0530

    postman-test_all.sh

--- day-trader-java-app/daytrader-postman-testing/test_all.sh
+++ day-trader-java-app/daytrader-postman-testing/test_all.sh
@@ -2,6 +2,10 @@
 #newman run AdvanceAPIs.postman_collection.json --reporters cli,junit --reporter-junit-export Results\jadvanceAPIResult.xml
 #newman run SimpleCURDAPIs.postman_collection.json --reporters cli,junit --reporter-junit-export Results\SimpleCURDAPIsResult.xml
 
-newman run accounts.postman_collection.json  --insecure --reporters cli,junit --reporter-junit-export Results\accountsResult.xml
-newman run portfolio.postman_collection.json --insecure --reporters cli,junit --reporter-junit-export Results\portfolioResult.xml
-newman run quotes.postman_collection.json    --insecure --reporters cli,junit --reporter-junit-export Results\quotesResult.xml
+newman run accounts_tables.postman_collection.json  --insecure --reporters cli,junit --reporter-junit-export Results\tables_accountsResult.xml
+newman run portfolio_tables.postman_collection.json --insecure --reporters cli,junit --reporter-junit-export Results\tables_portfolioResult.xml
+newman run quotes_tables.postman_collection.json    --insecure --reporters cli,junit --reporter-junit-export Results\tables_quotesResult.xml
+
+newman run accounts.postman_collection.json         --insecure --reporters cli,junit --reporter-junit-export Results\accountsResult.xml
+newman run portfolio.postman_collection.json        --insecure --reporters cli,junit --reporter-junit-export Results\portfolioResult.xml
+newman run quotes.postman_collection.json           --insecure --reporters cli,junit --reporter-junit-export Results\quotesResult.xml
