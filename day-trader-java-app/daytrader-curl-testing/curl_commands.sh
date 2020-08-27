cb0a671a94e6da67cec24c6dfaaec04cc18b71a5
Shibu TN <tnshibu@yahoo.com>
2020-08-27 15:00:40 +0530

    curl_commands.sh

--- day-trader-java-app/daytrader-curl-testing/curl_commands.sh
+++ day-trader-java-app/daytrader-curl-testing/curl_commands.sh
@@ -12,29 +12,34 @@ curl -k https://localhost:5443
 #-----------------
 #Accounts
 curl -k -L -X POST "https://localhost:1443/admin/recreateDBTables"
+#Portfolio
+curl -k  -L -X POST "https://localhost:3443/admin/recreateDBTables"
+#Quotes
+curl -k  -L -X POST "https://localhost:4443/admin/recreateDBTables"
+
+#=======================================================================
+#  POPULATE TABLES
+#-----------------
+#Accounts
 curl -k -L -X POST "https://localhost:1443/admin/tradeBuildDB?limit=10&offset=0"
 curl -k -L -X POST "https://localhost:1443/admin/tradeBuildDB?limit=10&offset=10"
 curl -k -L -X POST "https://localhost:1443/admin/tradeBuildDB?limit=10&offset=20"
 curl -k -L -X POST "https://localhost:1443/admin/tradeBuildDB?limit=10&offset=30"
 
 #Portfolio
-curl -k  -L -X POST "https://localhost:3443/admin/recreateDBTables"
 curl -k  -L -X POST "https://localhost:3443/admin/tradeBuildDB?limit=10&offset=0"
 curl -k  -L -X POST "https://localhost:3443/admin/tradeBuildDB?limit=10&offset=10"
 curl -k  -L -X POST "https://localhost:3443/admin/tradeBuildDB?limit=10&offset=20"
 curl -k  -L -X POST "https://localhost:3443/admin/tradeBuildDB?limit=10&offset=30"
 
 #Quotes
-curl -k  -L -X POST "https://localhost:4443/admin/recreateDBTables"
 curl -k  -L -X POST "https://localhost:4443/admin/tradeBuildDB?limit=10&offset=0"
 curl -k  -L -X POST "https://localhost:4443/admin/tradeBuildDB?limit=10&offset=10"
 curl -k  -L -X POST "https://localhost:4443/admin/tradeBuildDB?limit=10&offset=20"
 curl -k  -L -X POST "https://localhost:4443/admin/tradeBuildDB?limit=10&offset=30"
 
 #=======================================================================
-#  POPULATE TABLES
-#-----------------
-
+#generic testing
 curl -k  -L -X GET  "https://localhost:1443/accounts/uid:1/profiles"
 curl -k  -L -X GET  "https://localhost:1443/accounts/uid:1"
 
@@ -45,4 +50,6 @@ curl -k  -L -X GET  "https://localhost:1443/accounts/uid:1"
 #Quotes
 curl -k  -L -X GET  "https://localhost:4443/quotes/s:1"
 curl -k  -L -X GET  "https://localhost:4443/quotes?limit=10"
+#=======================================================================
+
 ########################################################################
