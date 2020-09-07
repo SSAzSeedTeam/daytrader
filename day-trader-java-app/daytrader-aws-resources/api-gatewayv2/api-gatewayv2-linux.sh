53f90dae8b1ebbe50ff33194a88e67ba82ff39f6
Shibu TN <tnshibu@yahoo.com>
2020-09-07 23:39:48 +0530

    eks

--- day-trader-java-app/daytrader-aws-resources/api-gatewayv2/api-gatewayv2-linux.sh
+++ day-trader-java-app/daytrader-aws-resources/api-gatewayv2/api-gatewayv2-linux.sh
@@ -9,43 +9,39 @@ aws apigatewayv2 --profile shibu_admin   delete-api --api-id $apiid
 #====================================================================================================
 #Create API gateway for Lambda function
 
-aws apigatewayv2 --profile shibu_admin   create-api --name shibu_dddd --protocol-type HTTP     --target arn:aws:lambda:us-east-2:560773393352:function:sspoc_daytrader_kubernetes_deploy
+aws apigatewayv2 --profile shibu_admin   create-api --name shibu_eeeee --protocol-type HTTP     --target arn:aws:lambda:us-east-2:560773393352:function:sspoc_daytrader_kubernetes_deploy
 
 apiid=`aws apigatewayv2 --profile shibu_admin   get-apis --query "Items[].ApiId" --output text`
 echo $apiid
 
-ApiId : ohxzpxhmzh
-ApiEndpoint": "https://ohxzpxhmzh.execute-api.us-east-2.amazonaws.com"
+#====================================================================================================
+aws apigatewayv2 --profile shibu_admin get-integrations --api-id $apiid
+
+#Note down Integration ID
+integrationId=`aws apigatewayv2 --profile shibu_admin get-integrations --api-id $apiid --query "Items[].IntegrationId" --output text`
+echo $integrationId
 
 #====================================================================================================
-aws apigatewayv2 --profile shibu_admin get-integrations --api-id ohxzpxhmzh
-#Note down Integration ID
-"IntegrationId": "mfyhwo6",
-"IntegrationMethod": "POST",
-#====================================================================================================
 #Update HTTP method to GET
-aws apigatewayv2 --profile shibu_admin update-integration --api-id ohxzpxhmzh --integration-id mfyhwo6 --integration-method GET
+aws apigatewayv2 --profile shibu_admin update-integration --api-id $apiid --integration-id $integrationId --integration-method GET
 #====================================================================================================
 #Allow API gateway to invoke Lambda
 #replace api-id below (and also stage name $default)
 
+# bash generate random 32 character alphanumeric string (lowercase only)
+randomId=`hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/urandom`
+
 aws lambda add-permission \
-  --profile shibu_admin
-  --statement-id ab21abf9-c85b-54d8-9ae3-a7efa0538be2 \
+  --profile shibu_admin \
+  --statement-id $randomId \
   --action lambda:InvokeFunction \
   --function-name "arn:aws:lambda:us-east-2:560773393352:function:sspoc_daytrader_kubernetes_deploy" \
   --principal apigateway.amazonaws.com \
-  --source-arn "arn:aws:execute-api:us-east-2:560773393352:ohxzpxhmzh/*/$default"
+  --source-arn "arn:aws:execute-api:us-east-2:560773393352:$apiid/*/$default"
   
-#above command in single line....
-aws lambda add-permission  --profile shibu_admin --statement-id ab21abf9-c85b-54d8-9ae3-a7efa0538be2   --action lambda:InvokeFunction   --function-name "arn:aws:lambda:us-east-2:560773393352:function:sspoc_daytrader_kubernetes_deploy"   --principal apigateway.amazonaws.com   --source-arn "arn:aws:execute-api:us-east-2:560773393352:$apiid/*/$default"
-output:
-{
-    "Statement": "{\"Sid\":\"ab21abf9-c85b-54d8-9ae3-a7efa0538be2\",\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"apigateway.amazonaws.com\"},\"Action\":\"lambda:InvokeFunction\",\"Resource\":\"arn:aws:lambda:us-east-2:560773393352:function:sspoc_daytrader_kubernetes_deploy\",\"Condition\":{\"ArnLike\":{\"AWS:SourceArn\":\"arn:aws:execute-api:us-east-2:560773393352:ohxzpxhmzh/*/$default\"}}}"
-}
 #====================================================================================================
 #invoke from command line
-curl -H "X-Amz-Invocation-Type: Event" https://ohxzpxhmzh.execute-api.us-east-2.amazonaws.com/
+curl -H "X-Amz-Invocation-Type: Event" https://$apiid.execute-api.us-east-2.amazonaws.com/
 #====================================================================================================
 
 #====================================================================================================
--- day-trader-java-app/daytrader-aws-resources/eks/eks/eks_parm.py
+++ day-trader-java-app/daytrader-aws-resources/eks/eks/eks_parm.py
@@ -8,27 +8,28 @@ version="1.0.0"
 ATTEMPTS=10
 WAIT_SEC=60
 
-
 #===============================
 #	VPC Information
 #===============================
 VPC_STACK_NAME="ss-poc-stack"
 VPC_TEMPLATE="https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-12-10/amazon-eks-vpc-sample.yaml"
-# VPC_TEMPLATE="D:\Vagrant-Projects\data\aws_scripts\yml\amazon-eks-vpc-private-subnets.yaml"
-
 
 #===============================
 #	EKS Cluster
 #===============================
 EKS_CLUSTER_NAME="ss-poc-cluster"
-# EKS_ROLE_ARN="arn:aws:iam::669678783832:role/cbd-eks-role"
-#EKS_ROLE_ARN="arn:aws:iam::560773393352:role/ss_poc_eks_cluster_policy"
-EKS_ROLE_ARN="arn:aws:iam::560773393352:role/sspoc_daytrader_eks_role"
+EKS_ROLE_ARN="arn:aws:iam::560773393352:role/sspoc_daytrader_lambda_role"
+
+#===============================
+#	Secondary User
+#===============================
+AWS_SECOND_USER_ARN="arn:aws:iam::560773393352:role/sspoc_daytrader_lambda_role"
+AWS_SECOND_USER_NAME="sspoc_daytrader_lambda_role"
+
 #===============================
 #	EKS Worker Nodes
 #===============================
 EKS_NODES_TEMPLATE="https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-12-10/amazon-eks-nodegroup.yaml"
-# EKS_NODES_TEMPLATE="D:\Vagrant-Projects\data\aws_scripts\yml\amazon-eks-nodegroup.yaml"
 EKS_NODES_STACK_NAME="ss-poc-nodes"
 EKS_NODE_GROUP_NAME="ss-poc-eks-nodes"
 EKS_NODE_AS_GROUP_MIN="1"
@@ -45,12 +46,3 @@ EKS_IMAGE_ID="ami-053cbe66e0033ebcf"
 EKS_KEY_NAME="MyKeyPair10"
 
 EKS_NODE_VOLUME_SIZE="20"
-
-#===============================
-#	Secondary User
-#===============================
-#AWS_SECOND_USER_ARN="arn:aws:iam::500946550028:user/shibu.narayanan"
-#AWS_SECOND_USER_NAME="shibu.narayanan"
-AWS_SECOND_USER_ARN="arn:aws:iam::560773393352:role/sspoc_daytrader_eks_role"
-AWS_SECOND_USER_NAME="sspoc_daytrader_eks_role"
-
