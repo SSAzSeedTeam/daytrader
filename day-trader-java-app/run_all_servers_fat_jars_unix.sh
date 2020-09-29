ea48fbfd24e0cb82efcc474cd496f16d47e2b297
Shibu TN <tnshibu@yahoo.com>
2020-09-29 10:51:54 +0530

    misc

--- day-trader-java-app/run_all_servers_fat_jars_unix.sh
+++ day-trader-java-app/run_all_servers_fat_jars_unix.sh
@@ -1,19 +1,19 @@
 cd daytrader-example-accountsrepo/daytrader-accountsapp/daytrader-accounts
-call ./env/external/springboot/bin/start_server.bat
+./env/external/springboot/bin/start_server.sh
 cd ../../..
 
 cd daytrader-example-gatewayrepo/daytrader-gatewayapp/daytrader-gateway
-call ./env/external/springboot/bin/start_server.bat
+./env/external/springboot/bin/start_server.sh
 cd ../../..
 
 cd daytrader-example-portfoliosrepo/daytrader-portfoliosapp/daytrader-portfolios
-call ./env/external/springboot/bin/start_server.bat
+./env/external/springboot/bin/start_server.sh
 cd ../../..
 
 cd daytrader-example-quotesrepo/daytrader-quotesapp/daytrader-quotes
-call ./env/external/springboot/bin/start_server.bat
+./env/external/springboot/bin/start_server.sh
 cd ../../..
 
 cd daytrader-example-webrepo/daytrader-webapp/daytrader-web
-call ./env/external/springboot/bin/start_server.bat
+./env/external/springboot/bin/start_server.sh
 cd ../../..
