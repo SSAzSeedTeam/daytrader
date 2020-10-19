91d12d8a2cc0351efcf9ed44bfd3e1f45776d130
Bharathi Somana <bharthi.somana@oracle.com>
2020-10-19 13:09:36 +0530

    [Add] Common navigation bar to available on the all microfrontends

--- day-trader-java-app/daytrader-web-micro-fe/daytrader-container-mfe/src/components/shared/LoginNavbar/LoginNavbar.js
+++ day-trader-java-app/daytrader-web-micro-fe/daytrader-container-mfe/src/components/shared/LoginNavbar/LoginNavbar.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './LoginNavbar.css'
 import { NavLink, withRouter } from 'react-router-dom'
 import axios from 'axios'
 import { LOCAL_GATEWAY_URL } from '../../../constants'
 
 const LoginNavbar = (props) => {
+  const [showLoginNavbar, setShowLoginNavbar] = useState(false)
   const userId = localStorage.getItem('userId');
   const handleLogOut = () => {
     axios.patch(`${LOCAL_GATEWAY_URL}/logout/${userId}`)
@@ -13,31 +14,39 @@ const LoginNavbar = (props) => {
         props.history.push('/login');
       })
   }
-  console.log('props', props)
-  let page = 'Home'
-  if (props.location.pathname !== '/TradingAndPortfolios') {
-    page = props.location.pathname.replace('/', '')
-  }
+
+  useEffect(() => {
+    if (props.location.pathname.includes('/trading')) {
+      setShowLoginNavbar(true)
+    } else {
+      setShowLoginNavbar(false)
+    }
+  }, [props.location.pathname])
+  
   return (
-    <div className='app-login-navbar-container'>
-      <div className='aln-contet'>
-        <div className='top-banner-container'>
-          <div className='left-side'>
-            {`DayTrader ${page}`}
-          </div>
-          <div className='right-side'>
-            DayTrader
+    <>
+      {showLoginNavbar && (
+        <div className='app-login-navbar-container'>
+          <div className='aln-contet'>
+            <div className='top-banner-container'>
+              <div className='left-side'>
+                DayTrader
+              </div>
+              <div className='right-side'>
+                DayTrader
+              </div>
+            </div>
+            <div className='aln-nav-links'>
+              <NavLink activeClassName='active-link' exact to='/trading'>Home</NavLink>
+              <NavLink activeClassName='active-link' to='/trading/accounts'>Account</NavLink>
+              <NavLink activeClassName='active-link' to='/trading/portfolio'>Portfolio</NavLink>
+              <NavLink activeClassName='active-link' to='/trading/quotes'>Quotes/Trade</NavLink>
+              <a href="#" onClick={handleLogOut}>Logoff</a>
+            </div>
           </div>
         </div>
-        <div className='aln-nav-links'>
-          <NavLink activeClassName='active-link' exact to='/trading'>Home</NavLink>
-          <NavLink activeClassName='active-link' to='/trading/accounts'>Account</NavLink>
-          <NavLink activeClassName='active-link' to='/trading/portfolio'>Portfolio</NavLink>
-          <NavLink activeClassName='active-link' to='/trading/quotes'>Quotes/Trade</NavLink>
-          <a href="#" onClick={handleLogOut}>Logoff</a>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
