c64e5ed23b43f78ebc28a3a5509ad51e48756d56
arunsaha <arunima.saha@oracle.com>
2020-11-06 16:06:39 +0530

    send email from outlook

---
+++ azure-functions/Email_sender_funcLA/src/main/java/com/functions/model/FileData.java
@@ -0,0 +1,32 @@
+package com.functions.model;
+
+import java.io.File;
+
+public class FileData {
+	
+	public String fileName;
+	public File file;
+	public String contentType;
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public File getFile() {
+		return file;
+	}
+	public void setFile(File file) {
+		this.file = file;
+	}
+	public String getContentType() {
+		return contentType;
+	}
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+	
+	
+	
+
+}
