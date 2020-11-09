c7ad0cd246eb93fd2aaa6076c6b29dfe75f03cf5
Shibu TN <tnshibu@yahoo.com>
2020-11-09 21:35:12 +0530

    mfe

--- day-trader-java-app/docker-compose-new-build.sh
+++ day-trader-java-app/docker-compose-new-build.sh
@@ -1,15 +1,21 @@
-cd daytrader-accounts                ; sudo ./docker-build.sh ; cd ..
-cd daytrader-gateway                 ; sudo ./docker-build.sh ; cd ..
-cd daytrader-portfolios              ; sudo ./docker-build.sh ; cd ..
-cd daytrader-quotes                  ; sudo ./docker-build.sh ; cd ..
-cd daytrader-web                     ; sudo ./docker-build.sh ; cd ..
-cd daytrader-report-generator        ; sudo ./docker-build.sh ; cd ..
-cd daytrader-onprem-exchange-rate    ; sudo ./docker-build.sh ; cd ..
-cd daytrader-trade-consumer          ; sudo ./docker-build.sh ; cd ..
-cd daytrader-trade-producer          ; sudo ./docker-build.sh ; cd ..
-cd daytrader-scdf-trade-generator    ; sudo ./docker-build.sh ; cd ..
-cd daytrader-scdf-trade-processor    ; sudo ./docker-build.sh ; cd ..
-cd daytrader-scdf-trade-validation   ; sudo ./docker-build.sh ; cd ..
+cd daytrader-accounts                                   ; sudo ./docker-build.sh ; cd ..
+cd daytrader-gateway                                    ; sudo ./docker-build.sh ; cd ..
+cd daytrader-portfolios                                 ; sudo ./docker-build.sh ; cd ..
+cd daytrader-quotes                                     ; sudo ./docker-build.sh ; cd ..
+cd daytrader-web                                        ; sudo ./docker-build.sh ; cd ..
+
+cd daytrader-web-micro-fe/daytrader-account-mfe         ; sudo ./docker-build.sh ; cd ../..
+cd daytrader-web-micro-fe/daytrader-container-mfe       ; sudo ./docker-build.sh ; cd ../..
+cd daytrader-web-micro-fe/daytrader-portfolios-mfe      ; sudo ./docker-build.sh ; cd ../..
+cd daytrader-web-micro-fe/daytrader-quotes-mfe          ; sudo ./docker-build.sh ; cd ../..
+
+cd daytrader-report-generator                           ; sudo ./docker-build.sh ; cd ..
+cd daytrader-onprem-exchange-rate                       ; sudo ./docker-build.sh ; cd ..
+cd daytrader-trade-consumer                             ; sudo ./docker-build.sh ; cd ..
+cd daytrader-trade-producer                             ; sudo ./docker-build.sh ; cd ..
+cd daytrader-scdf-trade-generator                       ; sudo ./docker-build.sh ; cd ..
+cd daytrader-scdf-trade-processor                       ; sudo ./docker-build.sh ; cd ..
+cd daytrader-scdf-trade-validation                      ; sudo ./docker-build.sh ; cd ..
 
 sudo docker images
 
