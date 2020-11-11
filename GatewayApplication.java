28857f848ad871f86986dd57a6f81b145d4cfca6
mahesh60-web <71712738+mahesh60-web@users.noreply.github.com>
2020-11-11 13:03:26 +0530

    Add files via upload
    redis cache changes

---
+++ GatewayApplication.java
@@ -0,0 +1,96 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.ofss.daytrader.gateway;
+
+import javax.sql.DataSource;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ContextResource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+/*import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;*/
+import org.springframework.boot.web.servlet.ServletComponentScan;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.ofss.daytrader.entities.AccountProfileDataBean;
+
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@EnableHystrix
+@EnableCaching
+//@ComponentScan("com.ofss.daytrader.gateway.cache")
+@ServletComponentScan(basePackages={"com.ofss.daytrader.web"})
+@SpringBootApplication
+public class GatewayApplication extends SpringBootServletInitializer {
+	
+	//	- Each microservice has their own private database (datasource)
+
+        //redis cache changes
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		
+		return new JedisConnectionFactory();
+	}
+	
+   //redis cache changes
+	@Bean
+	RedisTemplate<String, AccountProfileDataBean> redisTemplate(){
+		RedisTemplate<String, AccountProfileDataBean> redisTemplate=new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(jedisConnectionFactory());
+		return redisTemplate;
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GatewayApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	/*@Bean
+	public TomcatEmbeddedServletContainerFactory tomcatFactory() 
+	{
+		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory() 
+		{
+			@Override
+			protected TomcatEmbeddedServletContainer getTomcatEmbeddedServletContainer(Tomcat tomcat) 
+			{
+				tomcat.enableNaming();
+				return super.getTomcatEmbeddedServletContainer(tomcat);
+			}
+
+			@Override
+			protected void postProcessContext(Context context) {}
+		};
+		
+		return factory;
+	}*/
+}
+
