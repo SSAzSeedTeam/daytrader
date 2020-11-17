fa86295467a1b4db3ed29ba08739811182d4e040
BalachandraThambi <tbalachandra07@gmail.com>
2020-11-17 17:13:10 +0530

    utilities

---
+++ day-trader-java-app/daytrader-utilities/src/main/java/com/ofss/daytrader/core/jwt/MyUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.ofss.daytrader.core.jwt;
+
+import java.util.ArrayList;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+import org.springframework.security.core.userdetails.User;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService{
+
+	//@Autowired
+	//private AccountsProfileRepository accountsProfileRepository;
+	/*
+	 * PasswordEncoder encoder =
+	 * PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	 */
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		System.out.println("inside loadUserByUsername(): "+username);
+		//String pwd = accountsProfileRepository.getPwdByUserid(username);
+		//System.out.println("password---"+ pwd);
+		return new User(username,"A@1234567893",new ArrayList<>());
+		//return new User(username,pwd,new ArrayList<>());
+	}
+
+	
+}
