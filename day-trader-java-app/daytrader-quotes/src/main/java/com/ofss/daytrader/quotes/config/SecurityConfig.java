4f4bc90a22c7cce721274d9b0776004af74ceb46
BalachandraThambi <tbalachandra07@gmail.com>
2020-11-17 19:40:20 +0530

    added JWT classes in all services

---
+++ day-trader-java-app/daytrader-quotes/src/main/java/com/ofss/daytrader/quotes/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.ofss.daytrader.quotes.config;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.ofss.daytrader.quotes.utils.JwtFilterRequest;
+
+
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	  /*@Autowired
+	  private MyUserDetailsService myUserDetailsService;*/
+	  
+	  @Autowired
+	  private JwtFilterRequest jwtFilterRequest;
+	
+	  @Value("${jwt.secret}")
+		private String secret;
+		
+		
+		@Override
+		@Order(Ordered.HIGHEST_PRECEDENCE)
+	    public void configure(HttpSecurity http) throws Exception {
+	        http
+	        .csrf().disable()
+	            .authorizeRequests()
+	            .antMatchers("/login/*").authenticated()
+	            .anyRequest().permitAll()
+	            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	        
+	        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
+	    }
+	 
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManager();
+	}
+
+	/*@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(myUserDetailsService);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+				//new BCryptPasswordEncoder();//
+*/	//}
+}
---
+++ day-trader-java-app/daytrader-quotes/src/main/java/com/ofss/daytrader/quotes/utils/JwtFilterRequest.java
@@ -0,0 +1,100 @@
+package com.ofss.daytrader.quotes.utils;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.ofss.daytrader.quotes.service.MyUserDetailsService;
+
+import io.jsonwebtoken.SignatureException;
+
+
+@Component
+public class JwtFilterRequest extends OncePerRequestFilter{
+
+	@Autowired
+	private MyUserDetailsService myUserDetailsService;
+	
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+	
+	@SuppressWarnings("unused")
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("inside doFilterInternal");
+		String autherization = request.getHeader("Authorization");
+		System.out.println("autherization value - " + autherization);
+		String username = null;
+		String jwt = null;
+		
+		
+		String path = request.getRequestURI();
+		System.out.println(path);
+	    if (path.equals("/accounts")) {
+	    	filterChain.doFilter(request, response);
+	    	return;
+	    }
+	    
+		if (autherization == null) {
+			System.out.println("response value - " + HttpServletResponse.SC_FORBIDDEN);
+			response.sendError(HttpServletResponse.SC_FORBIDDEN);
+			//filterChain.doFilter(request, response);
+			return ;
+		}
+		
+		if(null!=autherization && autherization.startsWith("Bearer ")) {
+			System.out.println("if authorization");
+			jwt = autherization.substring(7);
+			System.out.println("jwt:"+jwt);
+			try {
+				username = jwtTokenUtil.getUsernameFromToken(jwt);
+			}
+			catch(SignatureException exc) {
+				System.out.println("invalid token ");
+				response.sendError(HttpServletResponse.SC_FORBIDDEN);
+				return ;
+			}
+			
+		}
+		// Once we get the token validate it.
+		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = this.myUserDetailsService.loadUserByUsername(username);
+			
+			System.out.println("userDetails - " + userDetails);
+			// if token is valid configure Spring Security to manually set
+			// authentication
+			if (jwtTokenUtil.validateToken(jwt, userDetails)) {
+				System.out.println("if validate Token");
+				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+				userDetails, null, userDetails.getAuthorities());
+				usernamePasswordAuthenticationToken
+				.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				// After setting the Authentication in the context, we specify
+				// that the current user is authenticated. So it passes the
+				// Spring Security Configurations successfully.
+				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+				filterChain.doFilter(request, response);
+				return;
+			}
+		}
+		System.out.println("end of do filter");
+		filterChain.doFilter(request, response);
+		//response.sendError(HttpServletResponse.SC_FORBIDDEN);
+		//return ;
+		//if (username == null) filterChain.doFilter(request, response);
+	}
+
+}
