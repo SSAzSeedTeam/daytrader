38e9ae8bbe9dbe15e2b30335ec999f719cdab4a4
Avinash Singh Kachhwaha <singhavinash857@gmail.com>
2020-11-24 14:48:50 +0530

    adding # instead / in sed command

--- day-trader-java-app/daytrader-web-micro-fe/daytrader-container-mfe/set_env_and_start_static.sh
+++ day-trader-java-app/daytrader-web-micro-fe/daytrader-container-mfe/set_env_and_start_static.sh
@@ -2,8 +2,8 @@ export GATEWAY_URL=$1
 export ACCOUNT_MFE_URL=$2
 export PORTFOLIO_MFE_URL=$3
 export QUOTES_MFE_URL=$4
-find "/usr/share/nginx/html" -name "index.html" -exec sed -i "s/GATEWAY_END_POINT_URL/$GATEWAY_URL/g" {} \;
-find "/usr/share/nginx/html" -name "index.html" -exec sed -i "s/REACT_APP_DAYTRADER_ACCOUNT_MFE_URL/$ACCOUNT_MFE_URL/g" {} \;
-find "/usr/share/nginx/html" -name "index.html" -exec sed -i "s/REACT_APP_DAYTRADER_PORTFOLIO_MFE_URL/$PORTFOLIO_MFE_URL/g" {} \;
-find "/usr/share/nginx/html" -name "index.html" -exec sed -i "s/REACT_APP_DAYTRADER_QUOTES_MFE_URL/$QUOTES_MFE_URL/g" {} \;
+find "/usr/share/nginx/html" -name "index.html" -exec sed -i "s#GATEWAY_END_POINT_URL#$GATEWAY_URL#g" {} \;
+find "/usr/share/nginx/html" -name "index.html" -exec sed -i "s#REACT_APP_DAYTRADER_ACCOUNT_MFE_URL#$ACCOUNT_MFE_URL#g" {} \;
+find "/usr/share/nginx/html" -name "index.html" -exec sed -i "s#REACT_APP_DAYTRADER_PORTFOLIO_MFE_URL#$PORTFOLIO_MFE_URL#g" {} \;
+find "/usr/share/nginx/html" -name "index.html" -exec sed -i "s#REACT_APP_DAYTRADER_QUOTES_MFE_URL#$QUOTES_MFE_URL#g" {} \;
 nginx
