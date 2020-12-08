87ade96d4470601e5a3223707827741e3f2e1d87
Avinash Singh Kachhwaha <singhavinash857@gmail.com>
2020-12-09 00:46:23 +0530

    removed hard coded localhost

--- day-trader-java-app/daytrader-web-micro-fe/daytrader-container-mfe/set_env_and_start.sh
+++ day-trader-java-app/daytrader-web-micro-fe/daytrader-container-mfe/set_env_and_start.sh
@@ -5,5 +5,5 @@ export QUOTES_MFE_URL=$4
 find . -name "index.html" -exec sed -i "s#GATEWAY_END_POINT_URL#$GATEWAY_URL#g" {} \;
 find . -name "index.html" -exec sed -i "s#%REACT_APP_DAYTRADER_ACCOUNT_MFE_URL%#$ACCOUNT_MFE_URL#g" {} \;
 find . -name "index.html" -exec sed -i "s#%REACT_APP_DAYTRADER_PORTFOLIO_MFE_URL%#$PORTFOLIO_MFE_URL#g" {} \;
-find . -name "index.html" -exec sed -i "s#%REACT_APP_DAYTRADER_QUOTES_MFE_URL%$QUOTES_MFE_URL#g" {} \;
+find . -name "index.html" -exec sed -i "s#%REACT_APP_DAYTRADER_QUOTES_MFE_URL%#$QUOTES_MFE_URL#g" {} \;
 npm start
