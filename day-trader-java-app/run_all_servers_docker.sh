c3fa885be025d5bab54254d80c97d890255580ab
Shibu TN <tnshibu@yahoo.com>
2020-12-13 07:54:23 +0530

    misc

--- day-trader-java-app/run_all_servers_docker.sh
+++ day-trader-java-app/run_all_servers_docker.sh
@@ -1,5 +1,7 @@
-cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-accountsrepo/daytrader-accountsapp/daytrader-accounts       ; ./docker-run.sh
-cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-gatewayrepo/daytrader-gatewayapp/daytrader-gateway          ; ./docker-run.sh
-cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-portfoliosrepo/daytrader-portfoliosapp/daytrader-portfolios ; ./docker-run.sh
-cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-quotesrepo/daytrader-quotesapp/daytrader-quotes             ; ./docker-run.sh
-cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-webrepo/daytrader-webapp/daytrader-web                      ; ./docker-run.sh
+#paths need to be fixed...
+
+#cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-accountsrepo/daytrader-accountsapp/daytrader-accounts       ; ./docker-run.sh
+#cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-gatewayrepo/daytrader-gatewayapp/daytrader-gateway          ; ./docker-run.sh
+#cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-portfoliosrepo/daytrader-portfoliosapp/daytrader-portfolios ; ./docker-run.sh
+#cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-quotesrepo/daytrader-quotesapp/daytrader-quotes             ; ./docker-run.sh
+#cd /ss-alphagx-repo-azure/GXEnablement/day-trader-java-app/daytrader-example-webrepo/daytrader-webapp/daytrader-web                      ; ./docker-run.sh
--- day-trader-java-app/run_all_servers_fat_jars_unix.sh
+++ day-trader-java-app/run_all_servers_fat_jars_unix.sh
@@ -1,19 +1,23 @@
-cd daytrader-example-accountsrepo/daytrader-accountsapp/daytrader-accounts
-./env/external/springboot/bin/start_server.sh
-cd ../../..
+#  DO NOT USE this script
+#  Go to individual project and run application start script from there
 
-cd daytrader-example-gatewayrepo/daytrader-gatewayapp/daytrader-gateway
-./env/external/springboot/bin/start_server.sh
-cd ../../..
-
-cd daytrader-example-portfoliosrepo/daytrader-portfoliosapp/daytrader-portfolios
-./env/external/springboot/bin/start_server.sh
-cd ../../..
-
-cd daytrader-example-quotesrepo/daytrader-quotesapp/daytrader-quotes
-./env/external/springboot/bin/start_server.sh
-cd ../../..
-
-cd daytrader-example-webrepo/daytrader-webapp/daytrader-web
-./env/external/springboot/bin/start_server.sh
-cd ../../..
+#cd daytrader-example-accountsrepo/daytrader-accountsapp/daytrader-accounts
+#./env/external/springboot/bin/start_server.sh
+#cd ../../..
+#
+#cd daytrader-example-gatewayrepo/daytrader-gatewayapp/daytrader-gateway
+#./env/external/springboot/bin/start_server.sh
+#cd ../../..
+#
+#cd daytrader-example-portfoliosrepo/daytrader-portfoliosapp/daytrader-portfolios
+#./env/external/springboot/bin/start_server.sh
+#cd ../../..
+#
+#cd daytrader-example-quotesrepo/daytrader-quotesapp/daytrader-quotes
+#./env/external/springboot/bin/start_server.sh
+#cd ../../..
+#
+#cd daytrader-example-webrepo/daytrader-webapp/daytrader-web
+#./env/external/springboot/bin/start_server.sh
+#cd ../../..
+#
